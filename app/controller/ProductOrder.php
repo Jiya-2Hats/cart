c378c659d73b18e67ae6155496a445c9994835d7
Jiya-2hats <jiya@2hatslogic.com>
2021-12-16 12:39:56 +0530

    Updated ProductOrder controller for vertical spacing

--- app/controller/ProductOrder.php
+++ app/controller/ProductOrder.php
@@ -22,12 +22,14 @@ class ProductOrder extends BaseController
             $getRequest = file_get_contents('php://input');
             $getData = json_decode($getRequest);
             $getData->orderStatus = ORDER_FAILURE_STATUS;
+
             $placeOrder = $this->orderModel->orderPlaced($getData);
             if ($placeOrder == true) {
                 $output = ['status' => ORDER_SUCCESS_MESSAGE];
             } else {
                 $output = ['status' => ORDER_FAILURE_MESSAGE];
             }
+
             json_encode($output);
         } catch (Exception $exception) {
             http_response_code(500);
