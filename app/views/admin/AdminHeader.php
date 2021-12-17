30b1792b816c494d560a716b2ef7dd10122964db
Jiya-2hats <jiya@2hatslogic.com>
2021-12-17 19:42:55 +0530

    Created AdminHeader for nav bar in admin page

--- app/views/admin/AdminHeader.php
+++ app/views/admin/AdminHeader.php
@@ -1,13 +1,13 @@
-<div class="container-fluid">
-    <div class="container">
-        <div class="row">
+<div class="container-fluid p-0">
+    <div class="container-fluid p-0">
+        <div class="row p-0">
             <div class="col-sm-12">
                 <nav class="navbar navbar-light bg-light pt-2 ">
                     <div class="container-fluid">
                         <h1>Cart</h1>
                         <div class=" col-sm-2 text-right">
-                            <a href="<?= BASE_URL ?>/admin/orderlist">Orders </a>
-                            <a href="<?= BASE_URL ?>/admin/changesettings">Settings</a>
+                            <a href="<?= BASE_URL ?>/admin/orderlist" class="px-2">Orders </a>
+                            <a href="<?= BASE_URL ?>/admin/changesettings" class="px-2">Settings</a>
 
                         </div>
                     </div>
--- app/views/admin/ChangeSettings.php
+++ app/views/admin/ChangeSettings.php
@@ -1,13 +1,31 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-12 pt-5">
-            <div class="card p-2">
-                <form action="<?= BASE_URL ?>/saveMailList">
-
-                </form>
-                <form action="<?= BASE_URL ?>/savekey">
-
-                </form>
+            <div class=" p-2 row">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <form action="<?= BASE_URL ?>/saveMailList">
+                            <div class="col-sm-12 mt-2">
+                                <label for="emailList">Email List</label>
+                                <div class="col-sm-12 my-2 ">
+                                    <textarea type="text" name="emailList" id="emailList" rows="1" class="col-sm-12 inputfield"></textarea>
+                                </div>
+                            </div>
+                            <input type="submit" name="submitList" id="submitList" value="Submit">
+                        </form>
+                    </div>
+                    <div class="col-sm-6">
+                        <form action="<?= BASE_URL ?>/savekey" class="col-sm-12">
+                            <div class="col-sm-12 mt-2">
+                                <label for="key"> Key</label>
+                                <div class="col-sm-12 my-2">
+                                    <input type="text" name="key" id="key" placeholder="Google API Key" class="col-sm-12 inputfield px-2 py-2" required>
+                                </div>
+                            </div>
+                            <input type="submit" name="submitKey" id="submitKey" value="Submit">
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
