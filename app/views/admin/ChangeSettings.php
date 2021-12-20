e47bb991127384cdf208c0fe97bc43a82c3af7a4
Jiya-2hats <jiya@2hatslogic.com>
2021-12-20 11:01:26 +0530

    Included view score as card expand

--- app/views/admin/ChangeSettings.php
+++ app/views/admin/ChangeSettings.php
@@ -5,25 +5,31 @@
                 <div class="row">
                     <div class="col-sm-6">
                         <form action="<?= BASE_URL ?>/admin/saveMailList" method="POST">
+                            <div class="col-sm-12 py-3">
+                                <h2>Fraud Email List</h2>
+                            </div>
                             <div class="col-sm-12 mt-2">
                                 <label for="emailList">Email List</label>
                                 <div class="col-sm-12 my-2 ">
-                                    <textarea type="text" name="emailList" id="emailList" rows="1" class="col-sm-12 inputfield"><?= $data['emailList'] ?></textarea>
+                                    <textarea type="text" name="emailList" id="emailList" rows="1" class="col-sm-12 p-2 inputfield"><?= $data['emailList'] ?></textarea>
                                 </div>
                             </div>
-                            <input type="submit" name="submitList" id="submitList" value="Submit">
+                            <input type="submit" name="submitList" id="submitList" class="btn btn-primary" value="Submit">
                         </form>
                     </div>
                     <div class="col-sm-6">
                         <form action="<?= BASE_URL ?>/admin/savekey" class="col-sm-12" method="POST">
-                            <div class="col-sm-12 mt-2">
+                            <div class="col-sm-12 py-3">
+                                <h2>Google Api key</h2>
+                            </div>
+                            <div class="col-sm-12 mt-2 ">
                                 <label for="key"> Key</label>
                                 <div class="col-sm-12 my-2">
 
-                                    <input type="text" name="key" id="key" placeholder="Google API Key" class="col-sm-12 inputfield px-2 py-2" value="<?= $data['apiKey']->apiKey ?>" required>
+                                    <input type="text" name="key" id="key" placeholder="Google API Key" class="col-sm-12 inputfield px-2 py-3" value="<?= $data['apiKey']->apiKey ?>" required>
                                 </div>
                             </div>
-                            <input type="submit" name="submitKey" id="submitKey" value="Submit">
+                            <input type="submit" name="submitKey" id="submitKey" class="btn btn-primary" value="Submit">
                         </form>
                     </div>
                 </div>
--- app/views/admin/Orders.php
+++ app/views/admin/Orders.php
@@ -7,14 +7,16 @@
                         <tr>
                             <th class="col-sm-1">Sl.No</th>
                             <th class="col-sm-2">Name</th>
-                            <th class="col-sm-2">Price</th>
-                            <th class="col-sm-3">Email</th>
-                            <th class="col-sm-2">Status</th>
-                            <th class="col-sm-2">Attack Score</th>
+                            <th class="col-sm-1">Price</th>
+                            <th class="col-sm-2">Email</th>
+                            <th class="col-sm-4">Shipping Address</th>
+                            <th class="col-sm-1">Status</th>
+                            <th class="col-sm-3">Violation</th>
                         </tr>
                     </thead>
                     <tbody> <?php
                             $orderList = $data['orderList'];
+
                             $i = 1;
                             foreach ($orderList as $orderItem) { ?>
                             <tr>
@@ -22,26 +24,29 @@
                                 <td><?= $orderItem->name ?></td>
                                 <td><?= $orderItem->amount ?></td>
                                 <td><?= $orderItem->email ?></td>
+                                <td><?= $orderItem->shipAddress ?></td>
                                 <td><?= $orderItem->orderStatus ?></td>
-                                <td><?= $orderItem->score ?><a class="decoration-none" data-bs-toggle="collapse" data-bs-target="#collapseExample<?= $i ?>" aria-expanded="false" aria-controls="collapseExample<?= $i ?>">View </a></td>
+                                <td><?= $orderItem->score ?>%<a class="decoration-none" data-bs-toggle="collapse" data-bs-target="#collapseExample<?= $i ?>" aria-expanded="false" aria-controls="collapseExample<?= $i ?>"> + </a></td>
 
                             </tr>
                             <tr>
-                                <td colspan="7">
-                                    <div class="collapse col-sm-12" id="collapseExample<?= $i ?>">
+                                <td colspan="7" class="px-2">
+                                    <div class="collapse col-sm-12 pb-3" id="collapseExample<?= $i ?>">
 
                                         <div class="col-sm-12">
-                                            <div class="row">
+                                            <div class="row px-1">
                                                 <?php foreach ($orderItem->violation as $key => $violationItem) : ?>
 
 
-                                                    <div class="col-sm-3 px-5" style="border: 1px solid #8080805e;">
+                                                    <div class="col-sm-3 px-5 border-grey">
                                                         <div class="row text-center">
                                                             <span> <?= ucwords(preg_replace('/(?<!\ )[A-Z]/', ' $0', $key)) ?> Violation</span>
                                                         </div>
+
                                                         <div class="row text-center pt-2">
                                                             <span><?= $violationItem ?></span>
                                                         </div>
+
 
                                                     </div>
 
